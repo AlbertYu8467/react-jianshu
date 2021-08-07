d61002af5edd751e3ea86b39f1d107967284364f
yuhu <13343589996@163.com>
2021-08-07 23:11:16 +0800

    虚拟DOM

--- todolist/src/TodoItem.js
+++ todolist/src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
   constructor(props){
@@ -12,6 +13,16 @@ class TodoItem extends Component {
   handleClick(){
      this.props.delItem(this.props.index)
   }
+}
+
+TodoItem.propTypes = {
+  content: PropTypes.string,
+  delItem: PropTypes.func.isRequired,
+  index: PropTypes.number
+}
+
+TodoItem.defaultProps = {
+  test: 'hello world'
 }
 
 export default TodoItem;
